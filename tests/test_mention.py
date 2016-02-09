58d180f2f09adeb8bcc42ddfc5982f0b2e0c8b1d
Axel Herzog <teddydestodes@human.trafficking,agency>
2016-02-09 22:56:02 +0100

    added unittests for mentions

---
+++ tests/test_mention.py
@@ -0,0 +1,24 @@
+import pytest
+from functools import partial
+
+from twtxt.mentions import format_mentions
+
+def mock_mention_format(name, url, expected_name, expected_url):
+    assert name == expected_name
+    assert url == expected_url
+    if name:
+        return '@' + name
+    else:
+        return name
+
+def test_format_mentions():
+    texts = {'No Mention': 'No Mention',
+             '@<SomeName http://some.url/twtxt.txt>': ('SomeName', 'http://some.url/twtxt.txt'),
+             '@<http://some.url/twtxt.txt>': (None, 'http://some.url/twtxt.txt'),
+             '@<SomeName>': '@<SomeName>',
+             '@SomeName': '@SomeName'}
+    for input, expected in texts.items():
+        if isinstance(expected, tuple):
+            format_mentions(input, partial(mock_mention_format, expected_name=expected[0], expected_url=expected[1]))
+        else:
+            assert expected == format_mentions(input, partial(mock_mention_format, expected_name=None, expected_url=None))
