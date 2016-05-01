a7cb85abba7b620c2e8e468d747d3baaa114d7f8
Dakotah Cote <puddinglord3@gmail.com>
2016-04-30 21:18:31 -0400

    Added unit tests Dakotah

--- tests/comp490_dakotah_unittests.py
+++ tests/comp490_dakotah_unittests.py
@@ -18,8 +18,12 @@ class myTestClass(unittest.TestCase):
 	def test_if_new_tweet_shows_up_in_timeline(self):
 		runner = CliRunner()
 		result = runner.invoke(twtxt.cli.tweet, ['-nt', 'timeline'], input='this is new')
-		self.assertNotEqual(runner.invoke(twtxt.cli.cli,['timeline']),None)
+		self.assertNotEqual(runner.invoke(twtxt.cli.cli,['timeline']), 0)
 	
-	def test_
+	def test_still_shows_normal_timeline_with_no_errors(self):
+		runner = CliRunner()
+		result = runner.invoke(twtxt.cli.tweet, ['timeline'], None)
+		self.assertNotEqual(runner.invoke(twtxt.cli.cli,['timeline']), 0)
+		
 if __name__ == '__main__':
 	unittest.main()
