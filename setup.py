b3f29c3426ff03ab713612fab6e627bfd2e58e09
buckket <felix@buckket.org>
2022-11-12 23:20:43 +0100

    Allow older version of python-humanize

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     install_requires=[
         'aiohttp>=3.8.1,<4',
         'python-dateutil>=2.6.1,<3',
-        'humanize>=4.0.0,<5',
+        'humanize>=3.1.0,<5',
         'click>=8.0.0,<9',
     ],
 
